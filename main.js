7bf010192c74a515c06c95666b519efd66f5c319
RaphaelleC <raphaelle.charrier@gmail.com>
2021-04-19 10:46:57 +0100

    2048 functions to initialize game

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const grid = document.querySelector('.grid')
 const width = 4
-const tiles = []
+const gridLength = width ** 2
+const domTiles = []
 const restartButton = document.querySelector('#restart')
-let cat = 0
-let dog = 0
+const cat = {
+  position: 0,
+}
+const tiles = []
 
 // ? Creating the grid.
 for (let index = 0; index < width ** 2; index++) {
@@ -11,56 +14,77 @@ for (let index = 0; index < width ** 2; index++) {
   grid.appendChild(div)
   // To visualise the grid's numbers.
   div.innerHTML = index
-  tiles.push(div)
+  domTiles.push(div)
 }
 
-tiles[cat].classList.add('cat')
+function initialiseTiles () {
+  const random1 = Math.floor(Math.random() * gridLength)
+  let random2 = Math.floor(Math.random() * gridLength)
+  if (random1 === random2) {
+    random2 = Math.abs(random2 - 1)
+  }
+  for (let index = 0; index < gridLength; index++){
+    if (index === random1 || index === random2) {
+      tiles[index] = 2
+    } else {
+      tiles[index] = 0
+    }
+  }
+}
+initialiseTiles()
 
-// ? Checking if a movement is possible (further direction and collision)
-function canMoveUp (position, width) {
-  if ((position - width) >= 0 && tiles[position - width].classList.length === 0) {
-    return true
-  } else {
-    return false
+function tilesValueToDomTiles () {
+  for (let index = 0; index < gridLength; index++) {
+    const domTile = domTiles[index]
+    domTile.classList.remove(...domTile.classList)
+    if (tiles[index] === 2) {
+      domTile.classList.add('cat')
+    }
   }
 }
-function canMoveRight (position, width) {
-  if ((position + 1) % width !== 0 && tiles[position + 1].classList.length === 0) {
-    return true
-  } else {
-    return false
-  }
-}
-function canMoveDown (position, width) {
-  if ((position + width) < (width ** 2) && tiles[position + width].classList.length === 0) {
-    return true
-  } else {
-    return false
-  }
-}
-function canMoveLeft (position, width) {
-  if (position % width !== 0 && tiles[position - 1].classList.length === 0) {
-    return true
-  } else {
-    return false
-  }
-}
+
+tilesValueToDomTiles()
+
 
 // ? Reset/restart button
 restartButton.addEventListener('click', () => {
-  tiles[cat].classList.remove('cat')
-  tiles[dog].classList.remove('dog')
-  cat = Math.floor(Math.random() * tiles.length)
-  dog = Math.floor(Math.random() * tiles.length)
-  if (cat !== dog) {
-    tiles[cat].classList.add('cat')
-    tiles[dog].classList.add('dog')
-  } else {
-    Math.abs(dog--)
-    tiles[cat].classList.add('cat')
-    tiles[dog].classList.add('dog')
-  }
+  initialiseTiles()
+  tilesValueToDomTiles()
 })
+
+// domTiles[cat.position].classList.add('cat')
+
+// ? Checking if a movement is possible (further direction and collision)
+function canMoveUp (tile, width) {
+  if ((tile.position - width) >= 0 && domTiles[tile.position - width].classList.length === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+function canMoveRight (tile, width) {
+  if ((tile.position + 1) % width !== 0 && domTiles[tile.position + 1].classList.length === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+function canMoveDown (tile, width) {
+  if ((tile.position + width) < (width ** 2) && domTiles[tile.position + width].classList.length === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+function canMoveLeft (tile, width) {
+  if (tile.position % width !== 0 && domTiles[tile.position - 1].classList.length === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
 
 // ? Moving the image/numbers with the arrows.
 document.addEventListener('keydown', (event) => {
@@ -68,27 +92,27 @@ document.addEventListener('keydown', (event) => {
 
   if (key === 'ArrowUp') {
     while (canMoveUp(cat, width)) {
-      tiles[cat].classList.remove('cat')
-      cat -= width
-      tiles[cat].classList.add('cat')
+      domTiles[cat.position].classList.remove('cat')
+      cat.position -= width
+      domTiles[cat.position].classList.add('cat')
     }
   } else if (key === 'ArrowRight') {
     while (canMoveRight(cat, width)) {
-      tiles[cat].classList.remove('cat')
-      cat++
-      tiles[cat].classList.add('cat')
+      domTiles[cat.position].classList.remove('cat')
+      cat.position++
+      domTiles[cat.position].classList.add('cat')
     }
   } else  if (key === 'ArrowDown') {
     while (canMoveDown(cat, width)) {
-      tiles[cat].classList.remove('cat')
-      cat += width
-      tiles[cat].classList.add('cat')
+      domTiles[cat.position].classList.remove('cat')
+      cat.position += width
+      domTiles[cat.position].classList.add('cat')
     }
   } else if (key === 'ArrowLeft') {
     while (canMoveLeft(cat, width)) {
-      tiles[cat].classList.remove('cat')
-      cat -= 1
-      tiles[cat].classList.add('cat')
+      domTiles[cat.position].classList.remove('cat')
+      cat.position -= 1
+      domTiles[cat.position].classList.add('cat')
     }
   }
 })
